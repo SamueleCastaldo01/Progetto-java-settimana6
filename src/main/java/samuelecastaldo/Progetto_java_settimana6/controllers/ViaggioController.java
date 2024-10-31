4deef3775c44344eccd6faa00220a1a9813fce06
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-10-31 17:40:35 +0100

    Crud per quanto riguarda viaggio funziona ed testato

---
+++ src/main/java/samuelecastaldo/Progetto_java_settimana6/controllers/ViaggioController.java
@@ -0,0 +1,60 @@
+package samuelecastaldo.Progetto_java_settimana6.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import samuelecastaldo.Progetto_java_settimana6.entities.Viaggio;
+import samuelecastaldo.Progetto_java_settimana6.exceptions.BadRequestException;
+import samuelecastaldo.Progetto_java_settimana6.payloads.NewViaggioDTO;
+import samuelecastaldo.Progetto_java_settimana6.services.ViaggioService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/viaggio")
+public class ViaggioController {
+
+    @Autowired
+    ViaggioService viaggioService;
+
+    @GetMapping
+    public List<Viaggio> findAll() {
+        return this.viaggioService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Viaggio findById(@PathVariable long id) {
+        return this.viaggioService.findById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Viaggio save(@RequestBody @Validated NewViaggioDTO body, BindingResult validationResult) {
+        if (validationResult.hasErrors()) {
+            String message = validationResult.getAllErrors().stream().map(objectError -> objectError.getDefaultMessage())
+                    .collect(Collectors.joining(". "));
+            throw new BadRequestException("Ci sono stati errori nel payload! " + message);
+        }
+
+        return this.viaggioService.save(body);
+    }
+
+    @PutMapping("/{id}")
+    public Viaggio findByIdAndUpdate(@PathVariable long id, @RequestBody @Validated NewViaggioDTO body, BindingResult validationResult) {
+        if (validationResult.hasErrors()) {
+            validationResult.getAllErrors().forEach(System.out::println);
+            throw new BadRequestException("Ci sono stati errori nel payload!");
+        }
+        // Ovunque ci sia un body bisognerebbe validarlo!
+        return this.viaggioService.findByIdAndUpdate(id, body);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void findByIdAndDelete(@PathVariable long id) {
+        this.viaggioService.findByIdAndDelete(id);
+    }
+}
---
+++ src/main/java/samuelecastaldo/Progetto_java_settimana6/services/ViaggioService.java
@@ -0,0 +1,51 @@
+package samuelecastaldo.Progetto_java_settimana6.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import samuelecastaldo.Progetto_java_settimana6.entities.Viaggio;
+import samuelecastaldo.Progetto_java_settimana6.exceptions.NotFoundException;
+import samuelecastaldo.Progetto_java_settimana6.payloads.NewViaggioDTO;
+import samuelecastaldo.Progetto_java_settimana6.repositories.ViaggioRepository;
+
+import java.util.List;
+
+@Service
+public class ViaggioService {
+
+    @Autowired
+    private ViaggioRepository viaggioRepository;
+
+    //GET --------------------------------------------
+    public List<Viaggio> findAll() {
+        return this.viaggioRepository.findAll();
+    }
+
+    public Viaggio findById(long id) {
+        return this.viaggioRepository.findById(id).orElseThrow(() -> new NotFoundException(id));
+    }
+
+    //POST --------------------------------------------
+    public Viaggio save(NewViaggioDTO body) {
+        Viaggio newAutore = new Viaggio(body.destinazione(), body.data(), body.stato());
+        return this.viaggioRepository.save(newAutore);
+    }
+
+    //PUT --------------------------------------------
+    public Viaggio findByIdAndUpdate(long id, NewViaggioDTO body) {
+        Viaggio found = this.findById(id);
+
+        found.setDestinazione(body.destinazione());
+        found.setStato(body.stato());
+        found.setData(body.data());
+
+        return this.viaggioRepository.save(found);
+    }
+
+    //DELETE --------------------------------------------
+    public void findByIdAndDelete(long id) {
+        Viaggio found = this.findById(id);
+        this.viaggioRepository.delete(found);
+    }
+}
