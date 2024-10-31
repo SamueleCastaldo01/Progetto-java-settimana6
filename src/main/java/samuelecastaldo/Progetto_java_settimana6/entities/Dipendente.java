2e217f461d2aa261747be41bba250d292d39267c
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-10-31 15:59:40 +0100

    creazione entity viaggio e dipendente

---
+++ src/main/java/samuelecastaldo/Progetto_java_settimana6/entities/Dipendente.java
@@ -0,0 +1,74 @@
+package samuelecastaldo.Progetto_java_settimana6.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "dipendenti")
+public class Dipendente {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String username;
+    private String nome;
+    private String cognome;
+    private String email;
+
+    public Dipendente() {}
+
+    public Dipendente(String username, String nome, String cognome, String email) {
+        this.username = username;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Dipendente{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", nome='" + nome + '\'' +
+                ", cognome='" + cognome + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
--- src/main/java/samuelecastaldo/Progetto_java_settimana6/entities/Viaggio.java
+++ src/main/java/samuelecastaldo/Progetto_java_settimana6/entities/Viaggio.java
@@ -1,5 +1,65 @@
 package samuelecastaldo.Progetto_java_settimana6.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "viaggi")
 public class Viaggio {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String destinazione;
+    private LocalDate data;
+    private String stato;
+
+    public Viaggio() {}
+
+    public Viaggio(String destinazione, LocalDate data, String stato) {
+        this.destinazione = destinazione;
+        this.data = data;
+        this.stato = stato;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getDestinazione() {
+        return destinazione;
+    }
+
+    public void setDestinazione(String destinazione) {
+        this.destinazione = destinazione;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public String getStato() {
+        return stato;
+    }
+
+    public void setStato(String stato) {
+        this.stato = stato;
+    }
+
+    @Override
+    public String toString() {
+        return "Viaggio{" +
+                "id=" + id +
+                ", destinazione='" + destinazione + '\'' +
+                ", data=" + data +
+                ", stato='" + stato + '\'' +
+                '}';
+    }
 }
